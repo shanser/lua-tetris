efc52c6f0372ed92020a3c55b1c6cc26ab53e22c
Stéphane Hanser <shanser@captive-studio.com>
2011-03-09 18:19:37 +0100

    Changing convetion, just to try

--- app/model/grille.lua
+++ app/model/grille.lua
@@ -16,59 +16,17 @@ function Grille.new()
     end
   end
   
-  function self.leTempsPasse()
-    piecesDeplacees = deplaceLesPieces()
-    if nombreDeCaseDeplacees() == 0 then
-      creeUneNouvellePiece()
-    end
-  end
-  
-  function self.caseEnXY(x, y)
-    for i,case in pairs(self.cases) do
-      if case.x == x and case.y == y then
-        return case
-      end
-    end
-  end
-  
-  function self.pieceEnXY(x, y)
-    local case = self.caseEnXY(x, y)
-    local piece = nil
-    if case then
-      piece = case.piece
-    end
-    return piece
-  end
-  
-  function deplaceLesPieces()
-    initialiseLesCases()
-    for i,case in pairs(self.cases) do
-      local caseDuDessous = self.caseEnXY(case.x, case.y + 1)
-      deplacePieceDeCaseEnCase(case, caseDuDessous)
-    end
-  end
-  
-  function initialiseLesCases()
+  local initialiseLesCases = function()
     for i,case in pairs(self.cases) do
       case.aEteDeplacee = false
     end
   end
   
-  function nombreDeCaseDeplacees()
-    compteur = 0
-    for i,case in pairs(self.cases) do
-      if case.aEteDeplacee then
-        compteur = compteur + 1
-      end
-    end
-    return compteur
+  local peutDeplacerDeCaseEnCase = function(case, caseDuDessous)
+    return caseDuDessous and not case.aEteDeplacee and caseDuDessous.piece == nil and case.piece
   end
-  
-  function creeUneNouvellePiece()
-    self.caseEnXY(5, 1).piece = Piece.new()
-  end
-  
-  function deplacePieceDeCaseEnCase(caseOrigine, caseDestination)
+
+  local deplacePieceDeCaseEnCase = function(caseOrigine, caseDestination)
     if not peutDeplacerDeCaseEnCase(caseOrigine, caseDestination) then
       return
     end
@@ -78,8 +36,50 @@ function Grille.new()
     caseDestination.aEteDeplacee = true
   end
   
-  function peutDeplacerDeCaseEnCase(case, caseDuDessous)
-    return caseDuDessous and not case.aEteDeplacee and caseDuDessous.piece == nil and case.piece
+  local deplaceLesPieces = function()
+    initialiseLesCases()
+    for i,case in pairs(self.cases) do
+      local caseDuDessous = self.caseEnXY(case.x, case.y + 1)
+      deplacePieceDeCaseEnCase(case, caseDuDessous)
+    end
+  end
+  
+  local nombreDeCaseDeplacees = function()
+    local compteur = 0
+    for i,case in pairs(self.cases) do
+      if case.aEteDeplacee then
+        compteur = compteur + 1
+      end
+    end
+    return compteur
+  end
+  
+  local creeUneNouvellePiece = function()
+    self.caseEnXY(5, 1).piece = Piece.new()
+  end
+  
+  self.leTempsPasse = function()
+    deplaceLesPieces()
+    if nombreDeCaseDeplacees() == 0 then
+      creeUneNouvellePiece()
+    end
+  end
+  
+  self.caseEnXY = function(x, y)
+    for i,case in pairs(self.cases) do
+      if case.x == x and case.y == y then
+        return case
+      end
+    end
+  end
+  
+  self.pieceEnXY = function(x, y)
+    local case = self.caseEnXY(x, y)
+    local piece = nil
+    if case then
+      piece = case.piece
+    end
+    return piece
   end
   
   return self
